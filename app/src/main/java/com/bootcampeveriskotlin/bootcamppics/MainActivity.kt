e20a9f9b1fc74083db8aca3e28ab5262610f18b7
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2021-03-10 11:03:55 -0300

    PickImageFromGallery function

--- app/src/main/java/com/bootcampeveriskotlin/bootcamppics/MainActivity.kt
+++ app/src/main/java/com/bootcampeveriskotlin/bootcamppics/MainActivity.kt
@@ -2,6 +2,8 @@ package com.bootcampeveriskotlin.bootcamppics
 
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -36,7 +38,7 @@ class MainActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(requestCode: Int,
                                             permissions: Array<out String>,
                                             grantResults: IntArray) {
-            when (requestCode){}{
+            when (requestCode){
                 PERMISSION_CODE -> {
                 if (grantResults.size > 0 && grantResults [0] == PackageManager.PERMISSION_GRANTED){
                     pickImageFromGalery()
@@ -49,6 +51,20 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun pickImageFromGalery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            image_view.setImageURI(data?.data)
+        }
+    }
+
     companion object{
         private val PERMISSION_CODE = 1000
         private val IMAGE_PICK_CODE = 1001
